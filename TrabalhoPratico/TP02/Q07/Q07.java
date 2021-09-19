576b0c4483f62c465b0ea6415a350891fce5302c
Bruno Faria <bruno.fariadevl@gmail.com>
2021-09-19 18:00:06 -0300

    v0.9.1.4 - TP02 Q07 resolvida - 19/09/2021

---
+++ TrabalhoPratico/TP02/Q07/Q07.java
@@ -0,0 +1,354 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+class Serie {
+    private String nome;
+    private String formato;
+    private String duracao;
+    private String paisOrigem;
+    private String idioma;
+    private String emissoraOriginal;
+    private String transmissaoOriginal;
+    private int numeroTemporadas;
+    private int numeroEpisodios;
+
+    Serie() {
+        this.nome = "";
+        this.formato = "";
+        this.duracao = "";
+        this.paisOrigem = "";
+        this.idioma = "";
+        this.emissoraOriginal = "";
+        this.transmissaoOriginal = "";
+        this.numeroTemporadas = 0;
+        this.numeroEpisodios = 0;
+    }
+
+    Serie(String nome, String formato, String duracao, String paisOrigem, String idioma,
+            String emissoraOriginal, String transmissaoOriginal, int numeroTemporadas, int numeroEpisodios) {
+        this.nome = nome;
+        this.formato = formato;
+        this.duracao = duracao;
+        this.paisOrigem = paisOrigem;
+        this.idioma = idioma;
+        this.emissoraOriginal = emissoraOriginal;
+        this.transmissaoOriginal = transmissaoOriginal;
+        this.numeroTemporadas = numeroTemporadas;
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    public Serie Clone(){
+        Serie clone = new Serie();
+        clone.nome = this.nome;
+        clone.formato = this.formato;
+        clone.duracao = this.duracao;
+        clone.paisOrigem = this.paisOrigem;
+        clone.idioma = this.idioma;
+        clone.emissoraOriginal = this.emissoraOriginal;
+        clone.transmissaoOriginal = this.transmissaoOriginal;
+        clone.numeroTemporadas = this.numeroTemporadas;
+        clone.numeroEpisodios = this.numeroEpisodios;
+
+        return clone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFormato() {
+        return formato;
+    }
+
+    public void setFormato(String formato) {
+        this.formato = formato;
+    }
+
+    public String getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(String duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getPaisOrigem() {
+        return paisOrigem;
+    }
+
+    public void setPaisOrigem(String paisOrigem) {
+        this.paisOrigem = paisOrigem;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public String getEmissoraOriginal() {
+        return emissoraOriginal;
+    }
+
+    public void setEmissoraOriginal(String emissoraOriginal) {
+        this.emissoraOriginal = emissoraOriginal;
+    }
+
+    public String getTransmissaoOriginal() {
+        return transmissaoOriginal;
+    }
+
+    public void setTransmissaoOriginal(String transmissaoOriginal) {
+        this.transmissaoOriginal = transmissaoOriginal;
+    }
+
+    public int getNumeroTemporadas() {
+        return numeroTemporadas;
+    }
+
+    public void setNumeroTemporadas(int numeroTemporadas) {
+        this.numeroTemporadas = numeroTemporadas;
+    }
+
+    public int getNumeroEpisodios() {
+        return numeroEpisodios;
+    }
+
+    public void setNumeroEpisodios(int numeroEpisodios) {
+        this.numeroEpisodios = numeroEpisodios;
+    }
+
+    String removeTags(String line) {
+        String newLine = "";
+
+        int i = 0;
+        while(i < line.length()) {
+            if(line.charAt(i) == '<') {
+                i++;
+                while(line.charAt(i) != '>') {
+                    i++;
+                }
+            } else {
+                newLine += line.charAt(i);
+            }
+            i++;
+        }
+
+        return newLine;
+    }
+
+    public void ler(String nomeArquivo) throws Exception {
+        InputStreamReader isr = new InputStreamReader(new FileInputStream(nomeArquivo));
+        BufferedReader br = new BufferedReader(isr);
+
+        int posPonto = 0;
+        for(int i = 0; i < nomeArquivo.length(); i++) {
+            if(nomeArquivo.charAt(i) == '.') {
+                posPonto = i;
+            }
+        }
+        String nome = nomeArquivo.substring(12, posPonto);
+        nome = nome.replace('_', ' ');
+        this.nome = nome;
+
+        while(!br.readLine().contains("Formato"));
+        this.formato = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("Duração"));
+        this.duracao = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("País de origem"));
+        this.paisOrigem = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("Idioma original"));
+        this.idioma = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("Emissora de televisão original"));
+        this.emissoraOriginal = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("Transmissão original"));
+        this.transmissaoOriginal = removeTags(br.readLine()).replaceAll("&#160;", "").replaceAll("&nbsp;", "").trim();
+
+        while(!br.readLine().contains("N.º de temporadas"));
+        String numTe = removeTags(br.readLine()).trim();
+        String guardaNum = "";
+        for(int i = 0; i < numTe.length(); i++) {
+            if(numTe.charAt(i) == '0' || numTe.charAt(i) == '1' || numTe.charAt(i) == '2' || numTe.charAt(i) == '3' || numTe.charAt(i) == '4' ||
+            numTe.charAt(i) == '5' || numTe.charAt(i) == '6' || numTe.charAt(i) == '7' || numTe.charAt(i) == '8' || numTe.charAt(i) == '9') {
+                guardaNum = guardaNum + numTe.charAt(i);
+            } else if(numTe.charAt(i) == '(' || numTe.charAt(i) == '-' || numTe.charAt(i) == '+' || numTe.charAt(i) == ' ') {
+                break;
+            }
+        }
+        int num = Integer.parseInt(guardaNum);
+        this.numeroTemporadas = num;
+
+        while(!br.readLine().contains("N.º de episódios"));
+        String numEp = removeTags(br.readLine()).trim();
+        guardaNum = "";
+        for(int i = 0; i < numEp.length(); i++) {
+            if(numEp.charAt(i) == '0' || numEp.charAt(i) == '1' || numEp.charAt(i) == '2' || numEp.charAt(i) == '3' || numEp.charAt(i) == '4' ||
+            numEp.charAt(i) == '5' || numEp.charAt(i) == '6' || numEp.charAt(i) == '7' || numEp.charAt(i) == '8' || numEp.charAt(i) == '9') {
+                guardaNum = guardaNum + numEp.charAt(i);
+            } else if(numEp.charAt(i) == '(' || numEp.charAt(i) == '-' || numEp.charAt(i) == '+' || numEp.charAt(i) == ' ') {
+                break;
+            }
+        }
+        num = Integer.parseInt(guardaNum);
+        this.numeroEpisodios = num;
+
+        br.close();
+    }
+    
+    public String toString() {
+        return nome + " " + formato + " " + duracao + " " + paisOrigem + " " + idioma + " " + emissoraOriginal + " "
+                + transmissaoOriginal + " " + numeroTemporadas + " " + numeroEpisodios;
+    }
+}
+
+// FIFO - FirstIn-FirstOut
+class FilaCircular {
+    private Serie[] arraySerie;
+    private int primeiro;
+    private int ultimo;
+
+    public FilaCircular() {
+        this(6);
+    }
+
+    public FilaCircular(int tamanho) {
+        arraySerie = new Serie[tamanho];
+        primeiro = ultimo = 0;
+    }
+
+    public void inserir(Serie serie) throws Exception {
+        if(((ultimo + 1) % arraySerie.length) == primeiro) {
+            remover();
+        }
+
+        // Inserir um elemento no final da fila
+        arraySerie[ultimo] = serie;
+        ultimo = (ultimo + 1) % arraySerie.length;
+    }
+
+    public void remover() throws Exception {
+        if(primeiro == ultimo) {
+            throw new Exception("Erro ao remover elemento.");
+        }
+
+        // Atualiza o valor da primeira posição retirando o primeiro item da Fila
+        primeiro = (primeiro + 1) % arraySerie.length;
+    }
+
+    public void mostrar() {
+        int i = primeiro;
+
+        while(i != ultimo) {
+            System.out.println(arraySerie[i]);
+            i = (i + 1) % arraySerie.length;
+        }
+
+    }
+
+    public int tamanho (){
+        int contador = 0;
+    
+        for(int i = primeiro; i != ultimo; i = ((i + 1) % arraySerie.length)) {
+           contador = contador + 1;
+        }
+
+        return contador;
+    }
+
+    public int mediaArredondada() {
+        double soma = 0;
+        int resultado = 0;
+        double contador = 0;
+
+        for(int i = primeiro; i != ultimo; i = ((i + 1) % arraySerie.length)) {
+            soma = soma + arraySerie[i].getNumeroTemporadas();
+            contador++;
+        }
+
+        resultado = (int)Math.round(soma / contador);
+
+        return resultado;
+    }
+}
+
+class Q07 {
+    public static Serie lerDados(String entrada) throws Exception {
+        Serie serie = new Serie(); 
+        String arquivo = "";
+
+        arquivo = "/tmp/series/";
+        arquivo += entrada; 
+
+        serie = new Serie();
+        serie.ler(arquivo);
+
+        return serie;
+    }
+
+    public static void tratarComando(String seriesEntrada, FilaCircular fila) throws Exception {
+        String[] aux = seriesEntrada.split(" ");
+
+        if(seriesEntrada.charAt(0) == 'I') {
+            if(fila.tamanho() < 5) {
+                fila.inserir(lerDados(aux[1]));
+                System.out.println(fila.mediaArredondada());
+            } else if(fila.tamanho() == 5) {
+                fila.remover();
+                fila.inserir(lerDados(aux[1]));
+                System.out.println(fila.mediaArredondada());
+            }
+        } else if(seriesEntrada.charAt(0) == 'R') {
+            fila.remover();
+        }
+    }
+
+    public static boolean isFim(String s) {
+        return (s.length() == 3 && s.charAt(0) == 'F' && s.charAt(1) == 'I' && s.charAt(2) == 'M');
+    }
+
+    public static void main(String[] args) throws Exception {
+        MyIO.setCharset("UTF-8");
+        FilaCircular fila = new FilaCircular();
+        // Inicialização váriaveis
+        String[] entrada = new String[1000];
+        int numEntrada = 0;
+        int contador = 0;
+
+        // Leitura da entrada padrao
+        do {
+            entrada[numEntrada] = MyIO.readLine();
+        } while (isFim(entrada[numEntrada++]) == false);
+        numEntrada--; // Desconsiderar a palavra FIM
+
+        for(int i = 0; i < numEntrada; i++) {
+            fila.inserir(lerDados(entrada[i]));
+//            fila.mostrar(); -> Teste para ver se a fila funciona
+            System.out.println(fila.mediaArredondada());
+//            System.out.println("--------------------");   -> Separar na hora de testar
+        }
+    
+        int quantidade = MyIO.readInt();
+        String[] seriesEntrada = new String[quantidade];
+
+        // Ler as demais entradas
+        do {
+            seriesEntrada[contador] = MyIO.readLine();
+            tratarComando(seriesEntrada[contador], fila);
+            contador++;
+        } while (contador < quantidade);
+
+    }
+}
